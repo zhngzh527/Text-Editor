79100aab9b6940e88767695d0a614660bc90d9a9
ZHONGCHENG ZHAO <zhongchengzhao@ZHONGCHENGs-MBP.lan>
2023-02-07 19:14:32 -0600

    plugins

--- client/src-sw.js
+++ client/src-sw.js
@@ -27,4 +27,18 @@ warmStrategyCache({
 registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
 // TODO: Implement asset caching
-registerRoute();
+registerRoute(
+  ({ request }) => ['style', 'script', 'worker'].includes(request.destination),
+  new StaleWhileRevalidate({
+    cacheName: 'asset-cache',
+    plugins: [
+      new CacheableResponsePlugin({
+        statuses: [0, 200],
+      }),
+      new ExpirationPlugin({
+        maxEntries: 80,
+        maxAgeSeconds: 30 * 24 * 60 * 60,
+      }),
+    ],
+  })
+);
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -18,12 +18,52 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        title: 'JATE',
+      }),
+
+      new InjectManifest({
+        swSrc: './src-sw.js',
+        swDest: 'src-sw.js',
+      }),
+
+      new WebpackPwaManifest({
+        fingerprints: false,
+        inject: true,
+        name: 'JUST ANOTHER TEXT EDITOR',
+        short_name: 'JATE',
+        description: 'Text editor in the browser',
+        background_color: '#225ca3',
+        theme_color: '#225ca3',
+        start_url: './',
+        publicPath: './',
+        icons: [
+          {
+            src: path.resolve('src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons'),
+          },
+        ],
+      }),
     ],
 
     module: {
       rules: [
-        
+        { test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+        { test: /\.m?js$/,
+          exclude: /node_modules/,
+
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+              plugins: [ '@babel/plugin-proposal-object-rest-spread','@babel/transform-runtime', ],
+            },
+          },
+        },
       ],
     },
   };
